3373e1f7f74adadd5eec0c82a51f45bcb0dbc777
kaanproject <kaanproject1@gmail.com>
2026-05-11 14:48:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+﻿const sorularDiv = document.getElementById("questions");
+
+if(sorularDiv){
+
+  for(let i=1; i<=20; i++){
+
+    sorularDiv.innerHTML += `
+      <div class="question">
+        <p>${i}. Günlük yaşamınızda çevre dostu davranıyor musunuz?</p>
+
+        <select>
+          <option value="1">Evet</option>
+          <option value="3">Bazen</option>
+          <option value="5">Hayır</option>
+        </select>
+      </div>
+    `;
+  }
+}
+
+function hesapla(){
+
+  let toplam = 0;
+
+  const selectler = document.querySelectorAll("select");
+
+  selectler.forEach(secim => {
+    toplam += Number(secim.value);
+  });
+
+  let sonuc = "";
+
+  if(toplam <= 30){
+    sonuc = "Karbon ayak iziniz çok düşük 🌱";
+  }
+  else if(toplam <= 60){
+    sonuc = "Karbon ayak iziniz orta seviyede 🌍";
+  }
+  else{
+    sonuc = "Karbon ayak iziniz yüksek ⚠️";
+  }
+
+  document.getElementById("sonuc").innerHTML = `
+    <h2>${sonuc}</h2>
+    <p>Toplam Puan: ${toplam}</p>
+  `;
+}
